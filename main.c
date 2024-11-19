bba72b80284ad39f2c3e687a423f8ddf3bb7a76a
ViniciusFrota07 <vfrota178@gmail.com>
2024-11-19 20:14:12 -0300

    Resolvi o exercicio

--- main.c
+++ main.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main()
+{
+    int lin, col;
+    int **matriz;
+    scanf("%d", &lin);
+    scanf("%d", &col);
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    if(lin == 0){
+        printf("[matriz vazia]");
+    }else{
 
+    matriz = (int **)malloc(lin * (sizeof(int *)));
+
+    for (int i = 0; i < lin; i++)
+    {
+        matriz[i] = (int *)malloc(col * sizeof(int));
+    }
+
+    for (int i = 0; i < lin; i++)
+    {
+        for (int j = 0; j < col; i++)
+        {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    for (int i = 0; i < lin; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("%d", matriz[i][j]);
+            if (j < col - 1)
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    for(int i=0; i<lin; i++){
+        free(matriz[i]);
+
+    }
+    free(matriz);
+    }
     return 0;
 }
